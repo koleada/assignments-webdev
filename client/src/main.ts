c94a7c30e67e3fee20e69876b6151e73a789acba
root <root@DESKTOP-75UVCAI.>
2024-10-20 16:50:38 -0400

    please work

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,7 +6,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index';
 
-router.addRoute({ path: '/', component: () => import('./pages/index.vue') })
+
 const app = createApp(App)
 
 app.use(router)
