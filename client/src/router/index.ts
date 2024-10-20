ce6b80e32d1aa0de5694765a4711557be28bfb26
root <root@DESKTOP-75UVCAI.>
2024-10-20 16:24:23 -0400

    debug routes

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 
+console.log("Auto-generated routes:", routes);
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
