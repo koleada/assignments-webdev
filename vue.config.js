f1c8da5a8d889ce7984737034d8f46ab14e9343d
root <root@DESKTOP-75UVCAI.>
2024-10-20 15:21:58 -0400

    still trying to deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/assignments-webdev/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/assignments-webdev/client/" : "/",
 };
