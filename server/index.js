e7c84d0b6e3ee7332df92273557469ade691b12c
koleada <koleniz.da@gmail.com>
2024-11-18 15:15:03 -0500

    backend work and API fetch stuff

--- server/index.js
+++ server/index.js
@@ -4,7 +4,17 @@ const PORT = 3000;
 const userController = require('./controllers/users');
 
 app.use(express.json());
+
+//this is basically how we use our client-side built to serve those files
 app.use(express.static(__dirname + "/dist"))
+
+// horrible CORS config so we dont get errors. this is Middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+})
 
 // this function is async, so code below it will be executed before this gets executed
 // err means there was an error from the async, data is the return data
@@ -17,8 +27,8 @@ app.get('/', (req, res, next) => {
 })
   .use("/api/v1/users", userController)
 
-  //catch all very useful
-  .get("*", (req, res, next) => {
+    // catch all
+   .get("*", (req, res, next) => {
     res.sendFile(__dirname + "/dist/index.html");
   })
 
